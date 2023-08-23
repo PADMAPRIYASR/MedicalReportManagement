079bf965ab7cac5d3fb3275e6bb353e671e4c2b5
PADMAPRIYASR <108685209+PADMAPRIYASR@users.noreply.github.com>
2023-08-23 10:17:21 +0530

    Add files via upload

---
+++ Medicalreport/src/main/java/com/example/demo/MedicalreportApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalreportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalreportApplication.class, args);
+	}
+
+}
---
+++ Medicalreport/src/main/java/com/example/demo/model/MedicalModel.java
@@ -0,0 +1,74 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+	@Table(name="medicalname")
+	public class MedicalModel {
+		@Id
+		@GeneratedValue(strategy=GenerationType.IDENTITY)
+		private int medicalId;
+		@Column(name="medicalname")
+		private String name;
+		private String address;
+		private String gender;
+		private String disease;
+		private String bloodgroup;
+		private String phonenumber;
+		private String doctorname;
+		public int getMedicalId() {
+			return medicalId;
+		}
+		public void setMedicalId(int medicalId) {
+			this.medicalId = medicalId;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public String getGender() {
+			return gender;
+		}
+		public void setGender(String gender) {
+			this.gender = gender;
+		}
+		public String getDisease() {
+			return disease;
+		}
+		public void setDisease(String disease) {
+			this.disease = disease;
+		}
+		public String getBloodgroup() {
+			return bloodgroup;
+		}
+		public void setBloodgroup(String bloodgroup) {
+			this.bloodgroup = bloodgroup;
+		}
+		public String getPhonenumber() {
+			return phonenumber;
+		}
+		public void setPhonenumber(String phonenumber) {
+			this.phonenumber = phonenumber;
+		}
+		public String getDoctorname() {
+			return doctorname;
+		}
+		public void setDoctorname(String doctorname) {
+			this.doctorname = doctorname;
+		}
+		
+}
